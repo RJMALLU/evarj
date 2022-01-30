c653800c42d97be221d611c199eee802702611e1
RJMALLU <98645849+RJMALLU@users.noreply.github.com>
2022-01-30 15:15:19 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -37,12 +37,12 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-            InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('𝐇𝐄𝐋𝐏', url=f"https://t.me/{temp.U_NAME}?start=help"),
+            InlineKeyboardButton('𝐆𝐑𝐎𝐔𝐏', url='https://t.me/cine_makotta')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
+            text=f"<b>𝐓𝐇𝐀𝐍𝐊𝐒 𝐅𝐈𝐑 𝐀𝐃𝐃𝐈𝐍𝐆 𝐌𝐄 {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
             reply_markup=reply_markup)
     else:
         settings = await get_settings(message.chat.id)
@@ -53,7 +53,7 @@ async def save_group(bot, message):
                         await (temp.MELCOW['welcome']).delete()
                     except:
                         pass
-                temp.MELCOW['welcome'] = await message.reply(f"<b>Hey , {u.mention}, Welcome to {message.chat.title}</b>")
+                temp.MELCOW['welcome'] = await message.reply(f"<b>𝐇𝐄𝐋𝐋𝐎 𝐌𝐘 𝐅𝐑𝐈𝐄𝐍𝐃 , {u.mention}, 𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 {message.chat.title}</b>")
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
